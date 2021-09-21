53662ed55b2f7a1c26e0d792ce54b8ec57daa9dd
Tim Beermann <beermann@osism.tech>
2021-09-21 16:29:46 +0200

    switch to github app (#2)
    Signed-off-by: Tim Beermann <beermann@osism.tech>

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-SECRET_TOKEN=$(curl -s \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token ${SECRET_GITHUB_PERSONAL_ACCCESS_TOKEN}" \
-  ${SECRET_GITHUB_API_URL}/orgs/${SECRET_GITHUB_ORG}/actions/runners/registration-token | jq -r .token)
-
+SECRET_TOKEN=$(python3 get_token.py)
 ./config.sh --name "${SECRET_NAME}" --token "${SECRET_TOKEN}" --url "${SECRET_GITHUB_URL}/${SECRET_GITHUB_ORG}" --unattended --replace
 ./run.sh &
 wait $!
